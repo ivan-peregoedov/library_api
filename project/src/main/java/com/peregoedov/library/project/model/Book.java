cfb1083c69d639da5d57fc08b9ce75a1a7f8602f
ivanistrator <peregoedov20@gmail.com>
2024-11-05 03:16:22 +0700

    Создана сущность Book

---
+++ project/src/main/java/com/peregoedov/library/project/model/Book.java
@@ -0,0 +1,28 @@
+package com.peregoedov.library.project.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Date publishedDate;
+}
