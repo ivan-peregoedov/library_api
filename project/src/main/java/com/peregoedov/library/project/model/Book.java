eaded27bcaff563b59a059ea755d34fadb51deba
ivanistrator <peregoedov20@gmail.com>
2024-11-05 04:58:41 +0700

    Add nullable

--- project/src/main/java/com/peregoedov/library/project/model/Book.java
+++ project/src/main/java/com/peregoedov/library/project/model/Book.java
@@ -2,6 +2,7 @@ package com.peregoedov.library.project.model;
 
 import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,7 +25,10 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String author;
+    @Column(nullable = false)
     private Date publishedDate;
 }
