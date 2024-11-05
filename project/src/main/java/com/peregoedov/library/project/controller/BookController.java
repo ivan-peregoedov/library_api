25cf21f482e7c843e9065b84362fe5ad9f6ca5bc
ivanistrator <peregoedov20@gmail.com>
2024-11-05 16:58:13 +0700

    Описание BookController с помощью Swagger

--- project/src/main/java/com/peregoedov/library/project/controller/BookController.java
+++ project/src/main/java/com/peregoedov/library/project/controller/BookController.java
@@ -18,6 +18,8 @@ import com.peregoedov.library.project.dto.BookDto;
 import com.peregoedov.library.project.model.Book;
 import com.peregoedov.library.project.service.BookService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -28,11 +30,15 @@ import lombok.RequiredArgsConstructor;
 public class BookController {
     private final BookService bookService;
 
+    @Operation(summary = "Get all books", description = "Returns a list of all books")
     @GetMapping
     public ResponseEntity<List<Book>> findAll() {
         return ResponseEntity.ok(bookService.findAll());
     }
 
+    @Operation(summary = "Get book by id", description = "Returns a book by id")
+    @ApiResponse(responseCode = "200", description = "The requested book")
+    @ApiResponse(responseCode = "404", description = "Book not found")
     @GetMapping("/{id}")
     public ResponseEntity<Book> findById(@PathVariable Long id) {
         try {
@@ -42,6 +48,8 @@ public class BookController {
         }
     }
 
+    @Operation(summary = "Create new book", description = "Creates a new book")
+    @ApiResponse(responseCode = "201", description = "Book created successfully")
     @PostMapping
     public ResponseEntity<Book> save(@RequestBody BookDto bookDto) {
         Book savedBook = bookService.save(Book.builder()
@@ -53,12 +61,18 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedBook);
     }
 
+    @Operation(summary = "Update book", description = "Updates a book")
+    @ApiResponse(responseCode = "200", description = "Book updated successfully")
+    @ApiResponse(responseCode = "404", description = "Book not found")
     @PutMapping
     public ResponseEntity<Book> update(@RequestBody Book book) {
         Book updatedBook = bookService.update(book);
         return ResponseEntity.status(HttpStatus.OK).body(updatedBook);
     }
 
+    @Operation(summary = "Delete book by id", description = "Deletes a book by id")
+    @ApiResponse(responseCode = "204", description = "Book deleted successfully")
+    @ApiResponse(responseCode = "404", description = "Book not found")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         bookService.deleteById(id);
